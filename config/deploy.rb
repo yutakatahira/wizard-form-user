94c6a19c16352cc4b9fd78b5e8ba0fabf5bd461a
katahira yuta <mgy.90.24.yu@gmail.com>
2020-03-14 20:16:54 +0900

    modified some line

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,6 @@ set :repo_url,  'git@github.com:yutakatahira/wizard-form-user.git'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-set :stage, :production
-
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.0' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
