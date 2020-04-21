00b66406dfdc8bc93455ceab22f31f9860a46342
katahira yuta <mgy.90.24.yu@gmail.com>
2020-04-21 14:37:59 +0900

    forgot to commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   helper_method :signed_in?
   before_action :current_user
 
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+  end
+
+  def production?
+    Rails.env.production?
+  end
 end
