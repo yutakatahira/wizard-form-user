446533751bab3ea1ac43cce6ede600f1bd57a19a
katahira yuta <mgy.90.24.yu@gmail.com>
2020-04-21 18:33:38 +0900

    modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == "admin" &&
-      password == "2222"
+      password == "admin"
     end
   end
 
